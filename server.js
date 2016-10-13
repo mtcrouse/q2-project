55d5aa8a3464f588c52633f4618a986148c29972
mtcrouse <mettacrouse@gmail.com>
2016-10-13 11:33:03 -0700

    add index.html, app.js, server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.disable('x-powered-by');
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan('short'));
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  if (app.get('env') !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
+
+module.exports = app;
