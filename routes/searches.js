4986eb5093e7b46c4da4b35d30919f79f9206efa
mtcrouse <mettacrouse@gmail.com>
2016-10-14 16:36:44 -0700

    update searches route

--- routes/searches.js
+++ routes/searches.js
@@ -8,28 +8,47 @@ const { camelizeKeys, decamelizeKeys } = require('humps');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+router.get('/searches', (req, res, next) => {
+  knex('searches')
+    .orderBy('id')
+    .then((rows) => {
+      const searches = camelizeKeys(rows);
+
+      res.send(searches);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
 router.post('/searches', (req, res, next) => {
   const { searchTerm } = req.body;
+  const count = 1;
 
   if (!searchTerm || !searchTerm.trim()) {
     return next(boom.create(400, 'SearchTerm must not be blank'));
   }
+
+  const insertSearch = { searchTerm };
 
   return knex('searches')
     .where('search_term', searchTerm)
     .first()
     .then((row) => {
       if (row) {
-        return next(boom.create(400, 'Search already exists'));
+        let existingCount = row.count;
+        existingCount += 1;
+        insertSearch.count = existingCount;
+        return knex('searches')
+          .update(decamelizeKeys(insertSearch))
+          .where('search_term', searchTerm);
+      } else {
+        insertSearch.count = count;
+        return knex('searches').insert(decamelizeKeys(insertSearch), '*');
       }
     })
     .then(() => {
-      return knex('searches').insert(decamelizeKeys(searchTerm), '*');
-    })
-    .then((rows) => {
-      const currentSearch = camelizeKeys(rows[0]);
-
-      res.send(currentSearch);
+      res.send(insertSearch);
     })
     .catch((err) => {
       next(err);
