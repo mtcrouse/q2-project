b8aa9c6e8f2482f52238bb0f776fbc708cf3a434
mtcrouse <mettacrouse@gmail.com>
2016-10-18 14:49:28 -0700

    integrate map with twitter stuff

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -1,11 +1,48 @@
 $(document).ready(() => {
   'use strict';
 
-  var socket = io();
+    let map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40, lng: -90},
+      zoom: 4,
+      disableDefaultUI: true,
+      zoomControl: true,
+      styles: [
+          {
+            featureType: 'all',
+            stylers: [
+              { hue: '#22aaaa' },
+            ]
+          },
+          {
+            featureType: 'all',
+            elementType: 'labels',
+            stylers: [
+              { visibility: 'off'}
+            ]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [
+              { visibility: 'off'}
+            ]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry.fill',
+            stylers: [
+              { color: '#63BDDB' },
+              {saturation: 20}
+            ]
+          }
+        ]
+      });
 
-  socket.on('tweety', function(msg){
-    console.log(msg);
-  });
+  // var socket = io();
+  //
+  // socket.on('tweety', function(msg){
+  //   console.log(msg);
+  // });
 
   $('#search-form').submit((event) => {
     event.preventDefault();
@@ -20,31 +57,51 @@ $(document).ready(() => {
 
         $('#search-term').val('');
 
-
+        // $body.append(tweets);
+        $body.append(`<div class="tweet"><p>${tweets}</p><div>`);
 
         for (let tweet of tweets.statuses) {
-          console.log(tweet);
-          $body.append(`<div class="tweet"><p>${JSON.stringify(tweet.text)}</p><div>`);
-        }
+            $body.append(`<div class="tweet"><p>${tweet}</p><div>`);
+          }
+
+      console.log(testData);
+      heatmap.setData(testData);
       })
       .fail(() => {
         console.log('Unable to retrieve tweets');
       });
 
+    $('#search-form').val('San Francisco');
     return false;
   });
 
-  $('#stream-form').submit((event) => {
-    event.preventDefault();
+  const heatmapData = [];
 
-    $('.tweet').remove();
+  let heatmap = new HeatmapOverlay(map,
+  {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    "radius": .1,
+    "maxOpacity": 1,
+    // scales the radius based on map zoom
+    "scaleRadius": true,
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries
+    //   (there will always be a red spot with useLocalExtremas true)
+    "useLocalExtrema": false,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+  }
+  );
 
-    $.getJSON('/tweets/stream')
-      .done((tweets) => {
-        console.log(tweets);
-      })
-      .fail(() => {
-        console.log('Unable to retrieve tweets');
-      });
-  });
+  var testData = {
+  max: 1,
+  data: []
+  };
+
+  heatmap.setData(testData);
+
 });
--- server.js
+++ server.js
@@ -24,13 +24,13 @@ app.use(morgan('short'));
 app.use(express.static(path.join('public')));
 
 // CSRF protection
-app.use((req, res, next) => {
-  if (/json/.test(req.get('Accept'))) {
-    return next();
-  }
-
-  res.sendStatus(406);
-});
+// app.use((req, res, next) => {
+//   if (/json/.test(req.get('Accept'))) {
+//     return next();
+//   }
+//
+//   res.sendStatus(406);
+// });
 
 const users = require('./routes/users');
 const searches = require('./routes/searches');
