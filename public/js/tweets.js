c8390d03c3e5e957da1e5a872182ae921f5a2dd8
smk291 <s.marius.kurland@gmail.com>
2016-10-18 15:56:59 -0700

    working heatmap

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -1,6 +1,11 @@
 $(document).ready(() => {
   'use strict';
 
+var testData = {
+  max: 1,
+  data: []
+};
+
     let map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 40, lng: -90},
       zoom: 4,
@@ -38,10 +43,52 @@ $(document).ready(() => {
         ]
       });
 
+
+const heatmap = new HeatmapOverlay(map, 
+  {
+    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+    "radius": 1,
+    "maxOpacity": 1, 
+    // scales the radius based on map zoom
+    "scaleRadius": true, 
+    // if set to false the heatmap uses the global maximum for colorization
+    // if activated: uses the data maximum within the current map boundaries 
+    //   (there will always be a red spot with useLocalExtremas true)
+    "useLocalExtrema": false,
+    // which field name in your data represents the latitude - default "lat"
+    latField: 'lat',
+    // which field name in your data represents the longitude - default "lng"
+    lngField: 'lng',
+    // which field name in your data represents the data value - default "value"
+    valueField: 'count'
+  }
+);
+
   var socket = io();
 
   socket.on('tweety', function(msg){
     console.log(msg);
+    // const searchTerm = $('#search-term').val().trim();
+
+    // $.getJSON(`/tweets/${searchTerm}`)
+    //   .done((tweets) => {
+    //     const $body = $('body');
+
+    //     $('#search-term').val('');
+
+    //     for (let tweet of tweets.statuses) {
+    //         if (tweet.geo !== null){
+    //           $body.append(`<div class="tweet"><p>${tweet.geo.coordinates}</p><div>`);
+    //           let coords = tweet.geo.coordinates;
+    //           testData.data.push({lat: coords[0], lng: coords[1], count: 1});
+    //           console.log(testData);
+    //         }
+    //       }
+    // heatmap.setData(testData);
+    //   })
+    //   .fail(() => {
+    //     console.log('Unable to retrieve tweets');
+    //   });
   });
 
   $('#search-form').submit((event) => {
@@ -58,9 +105,14 @@ $(document).ready(() => {
         $('#search-term').val('');
 
         for (let tweet of tweets.statuses) {
-            $body.append(`<div class="tweet"><p>${tweet.text}</p><div>`);
+            if (tweet.geo !== null){
+              $body.append(`<div class="tweet"><p>${tweet.geo.coordinates}</p><div>`);
+              let coords = tweet.geo.coordinates;
+              testData.data.push({lat: coords[0], lng: coords[1], count: 1});
+              console.log(testData);
+            }
           }
-
+    heatmap.setData(testData);
       })
       .fail(() => {
         console.log('Unable to retrieve tweets');
@@ -68,7 +120,70 @@ $(document).ready(() => {
 
     return false;
   });
-
-
-
 });
+
+
+
+
+//   $('#search-form').submit((event) => {
+//     event.preventDefault();
+
+//     $('.tweet').remove();
+
+//     const searchTerm = $('#search-term').val();
+
+//     $.getJSON(`/tweets/${searchTerm}`)
+//       .done((tweets) => {
+//         const $body = $('body');
+
+//         $body.append(tweets);
+//         $body.append(`<div class="tweet"><p>${tweets}</p><div>`);
+
+//         for (let tweet of tweets.statuses) {
+//           if (tweet.coordinates !== null) {
+//             $body.append(`<div class="tweet"><p>${tweet}</p><div>`);
+//             $body.append(`<div class="tweet"><p>${tweet.coordinates.lat}</p><div>`);
+//             $body.append(`<div class="tweet"><p>${tweet.coordinates.lng}</p><div>`);
+//             console.log(coordinates);
+//           }
+//           if (tweet.user.location){
+//             let theaddress = tweet.user.location
+//             theaddress = theaddress.split('');
+       
+//             theaddress = theaddress.map(function(val){
+//               if (val === ' '){
+//                 return '+'
+//               } else {
+//                 return val
+//               }
+//             });
+//             theaddress = theaddress.join('');
+//             let $xhr = $.getJSON(`https://maps.googleapis.com/maps/api/geocode/json?address=${theaddress}&key=AIzaSyD9Eqb-zTPSDLCscIVgyP2RvYL3Y4GfxUk`);
+//             $xhr.done(function(xhrdata) {
+//               console.log(xhrdata);
+//               if (xhrdata !== undefined && xhrdata.results[0].formatted_address !== undefined){
+//                 let address = xhrdata.results[0].formatted_address;
+//                 let lat = xhrdata.results[0].geometry.location.lat;
+//                 let lng = xhrdata.results[0].geometry.location.lng;
+//                 let newData = {lat: lat, lng: lng, count: 1}
+//                 let loc = testData.data.indexOf(newData);
+//                 if (loc !== -1) {
+//                   console.log('it\'s there')
+//                   testData.data[loc].count++;
+//                   console.log(testData.data[loc].count);
+//                 } else{
+//                   testData.data.push({lat: lat, lng: lng, count: 1});
+//                 }
+//               }
+//             })
+//           }
+//       console.log(testData);
+//       heatmap.setData(testData);
+//       }
+//     })
+//       .fail(() => {
+//         console.log('Unable to retrieve tweets');
+//       });
+
+//     return false
+// });
