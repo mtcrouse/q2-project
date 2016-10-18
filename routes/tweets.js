861c15b093d5167b2aabc3b34e9662f87d66d7cf
smk291 <s.marius.kurland@gmail.com>
2016-10-18 09:04:29 -0700

    Update tweets.js

--- routes/tweets.js
+++ routes/tweets.js
@@ -7,10 +7,10 @@ var Twitter = require('twitter');
 const router = express.Router();
 
 var client = new Twitter({
-  consumer_key: 'tZSJ5ZiluwuAMrd8ihMHIxVcn',
-  consumer_secret: 'AIf3u6l0i3Ms0brXNdwVthebbnbb4yAxLOASnb6giA8S4PdjHJ',
-  access_token_key: '2660235607-h2AZhOaTY800pBxPsl0EtlMQS8lF6ml3gJLCbLq',
-  access_token_secret: 'XKbQ7vQylXlPk19tnujmn1OAyiiUeCE2fp2yX8jj5mTfa'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 });
 
 router.get('/tweets/stream', (req, res, next) => {
