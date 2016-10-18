2efcd535aba0a9dc08af60d7c1e18c3185144bf6
mtcrouse <mettacrouse@gmail.com>
2016-10-18 15:10:21 -0700

    get map to display and search to work -- need to add heatmap functionality

--- public/js/tweets.js
+++ public/js/tweets.js
@@ -38,11 +38,11 @@ $(document).ready(() => {
         ]
       });
 
-  // var socket = io();
-  //
-  // socket.on('tweety', function(msg){
-  //   console.log(msg);
-  // });
+  var socket = io();
+
+  socket.on('tweety', function(msg){
+    console.log(msg);
+  });
 
   $('#search-form').submit((event) => {
     event.preventDefault();
@@ -57,51 +57,18 @@ $(document).ready(() => {
 
         $('#search-term').val('');
 
-        // $body.append(tweets);
-        $body.append(`<div class="tweet"><p>${tweets}</p><div>`);
-
         for (let tweet of tweets.statuses) {
-            $body.append(`<div class="tweet"><p>${tweet}</p><div>`);
+            $body.append(`<div class="tweet"><p>${tweet.text}</p><div>`);
           }
 
-      console.log(testData);
-      heatmap.setData(testData);
       })
       .fail(() => {
         console.log('Unable to retrieve tweets');
       });
 
-    $('#search-form').val('San Francisco');
     return false;
   });
 
-  const heatmapData = [];
 
-  let heatmap = new HeatmapOverlay(map,
-  {
-    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-    "radius": .1,
-    "maxOpacity": 1,
-    // scales the radius based on map zoom
-    "scaleRadius": true,
-    // if set to false the heatmap uses the global maximum for colorization
-    // if activated: uses the data maximum within the current map boundaries
-    //   (there will always be a red spot with useLocalExtremas true)
-    "useLocalExtrema": false,
-    // which field name in your data represents the latitude - default "lat"
-    latField: 'lat',
-    // which field name in your data represents the longitude - default "lng"
-    lngField: 'lng',
-    // which field name in your data represents the data value - default "value"
-    valueField: 'count'
-  }
-  );
-
-  var testData = {
-  max: 1,
-  data: []
-  };
-
-  heatmap.setData(testData);
 
 });
